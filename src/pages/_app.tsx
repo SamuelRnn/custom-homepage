c49a9fdf6297d8ea253117f7310cc8ca56d7bca4
SamuelRnn <ricardo.reinn@outlook.com>
2023-04-24 14:39:15 -0500

    loader

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Didact_Gothic } from 'next/font/google'
+import { motion, AnimatePresence } from 'framer-motion'
+import { useEffect, useState } from 'react'
 
 const didact_gothic = Didact_Gothic({
 	subsets: ['latin'],
@@ -10,9 +12,33 @@ const didact_gothic = Didact_Gothic({
 })
 
 export default function App({ Component, pageProps }: AppProps) {
+	const [visible, setVisible] = useState(true)
+	useEffect(() => {
+		setVisible(false)
+	}, [])
 	return (
-		<main className={`${didact_gothic.variable} font-main text-zinc-300`}>
-			<Component {...pageProps} />
-		</main>
+		<>
+			<main className={`${didact_gothic.variable} font-main text-zinc-300`}>
+				<Component {...pageProps} />
+			</main>
+			<AnimatePresence>
+				{visible && (
+					<motion.div
+						exit={{ y: '-100%' }}
+						transition={{ type: 'spring', bounce: 0, duration: 1.4, delay: 0.1 }}
+						className="bg-zinc-900 fixed w-full h-screen z-[1000] loader-screen"
+					/>
+				)}
+			</AnimatePresence>
+			<AnimatePresence>
+				{visible && (
+					<motion.div
+						exit={{ y: '-100%' }}
+						transition={{ type: 'spring', bounce: 0, duration: 1 }}
+						className="bg-main-black fixed w-full h-screen z-[1000] loader-screen"
+					/>
+				)}
+			</AnimatePresence>
+		</>
 	)
 }
